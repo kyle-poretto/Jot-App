657ef260b6a5252c54c942a9465756ede2f92d00
kyle-poretto <kyleporetto@gmail.com>
2018-03-02 06:58:41 -0500

    fixed omniauth bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,14 +20,26 @@ class User < ApplicationRecord
 
   #Follows a user :)
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+
+
   def follow(other_user)
     if following?(other_user)
-      raise "what the fuck man"
+      raise "whattup man"
     else 
       following << other_user
     end
     
   end
+
 
   #Unfollows a user :(
 
@@ -51,6 +63,7 @@ class User < ApplicationRecord
   def low_following_count?
     self.following.count < 10
   end
+
 
 end
 
